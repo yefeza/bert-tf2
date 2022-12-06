9850b58ce53276cc360832c09b11be02bc0e486d
Yeison Fernandez <yeison@yeisonpc>
2022-12-05 21:13:34 -0400

    tf2 squad

--- run_squad.py
+++ run_squad.py
@@ -1137,7 +1137,7 @@ def main(_):
 
   tpu_cluster_resolver = None
   if FLAGS.use_tpu and FLAGS.tpu_name:
-    tpu_cluster_resolver = tf.contrib.cluster_resolver.TPUClusterResolver(
+    tpu_cluster_resolver = tf.distribute.cluster_resolver.TPUClusterResolver(
         FLAGS.tpu_name, zone=FLAGS.tpu_zone, project=FLAGS.gcp_project)
 
   is_per_host = tf.contrib.tpu.InputPipelineConfig.PER_HOST_V2
